26ea95936e29240bae09a47fe9964c0ed8f8214b
Chinito4852 <tristinowong@gmail.com>
2023-02-27 10:31:31 -0800

    Adding AWS lambda packages

--- sweet-tweets-api/Program.cs
+++ sweet-tweets-api/Program.cs
@@ -12,6 +12,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddTransient<TweetRepo>();
+builder.Services.AddAWSLambdaHosting(LambdaEventSource.HttpApi);
 
 var app = builder.Build();
 
